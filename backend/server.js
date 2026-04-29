94a0d2ab57a596fb9a47eb2541f0f81e44b9da66
Manan Patel <155524288+manan2324@users.noreply.github.com>
2026-04-29 17:03:13 +0530

    Apply suggestions from code review
    Co-authored-by: Manan Patel <155524288+manan2324@users.noreply.github.com>

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: process.env.FRONTEND_URL || "http://localhost:5173" ,
     credentials: true,
   }),
 );
